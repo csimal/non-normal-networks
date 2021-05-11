9f4138c43a89f77896fabb7cf2292ba92cf53659
csimal <cedric.simal@student.unamur.be>
2021-05-11 17:52:50 +0200

    updated notebook

--- bottleneck.jl
+++ bottleneck.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.14.3
+# v0.14.5
 
 using Markdown
 using InteractiveUtils
@@ -38,15 +38,19 @@ function henrici(A::AbstractMatrix; normalize=true)
 end
 
 # ╔═╡ 55e9a97f-e770-47ea-a28b-4aa671f163ff
-function h_exact(n, ε)
-	return sqrt(n-1 + ε^2 - n*ε^(2/n)) / sqrt(n-1+ε^2)
+function h_exact(n, ε; normalize = true)
+	if normalize
+		return sqrt(n-1 + ε^2 - n*ε^(2/n)) / sqrt(n-1+ε^2)
+	else
+		return sqrt(n-1 + ε^2 - n*ε^(2/n))
+	end
 end
 
 # ╔═╡ 04c27d15-bc70-45de-93ac-eb4ef7c6de71
-h_(n) = x -> h_exact(n,x)
+h_(n; normalize=true) = x -> h_exact(n,x, normalize=normalize)
 
 # ╔═╡ 8fa1ff8d-2da3-4299-a072-b07e3323df65
-@bind M Slider(5:100)
+@bind M Slider(5:100, show_value=true)
 
 # ╔═╡ 782234ec-7ff1-4835-b3e3-d76aa32bfbc4
 begin
@@ -56,7 +60,7 @@ end
 
 # ╔═╡ 3db7c5d9-f362-41af-b4f5-2dd8957ef981
 begin
-	local labels = Dict((5,1)=>L"\varepsilon")
+	local labels = Dict((5,1)=> L"\varepsilon")
 	graphplot(g, 
 		edgelabel=labels,
 		fontsize=12,
@@ -80,7 +84,7 @@ begin
 		label="Analytical",
 		xlabel=L"\varepsilon",
 		ylabel=L"\hat{h}",
-		title="Normalised departure from normality for N=$(M)",
+		title="Normalised departure from normality for N =$(M)",
 		xscale=:log10,
 		#yscale=:log10
 	)
@@ -123,7 +127,7 @@ begin
 		xscale=:log10
 	)
 	for i in 1:length(ε)
-		plot!(p, 2:(n+1), h[i,:], label="ε = 10^$(ε[i])")
+		plot!(p, 2:(n+1), h[i,:], label=L"\varepsilon = 10^{%$(ε[i])}")
 	end
 	#savefig(p, "images/bottleneck-henrici-2.pdf")
 	p
@@ -133,17 +137,55 @@ end
 begin
 	local ε = 10.0 .^ -LinRange(1.0,16.0,100)
 	local N = 10 .^ LinRange(1.0,16.0, 100)
-	contour(N, ε, (x,y) -> log10.(h_exact(x,y)),
+	contour(N, ε, (x,y) -> log10(h_exact(x,y)),
 		xscale=:log10,
 		yscale=:log10,
-		cscale=:log10,
 		fill=true,
 		xlabel=L"N",
 		ylabel=L"\varepsilon",
 		title=L"\log_{10} \hat{h}",
-		levels=50
+		levels=50,
+		grid=false
 	)
 	#savefig("images/bottleneck-contour.pdf")
+end
+
+# ╔═╡ 00ec2f66-38b1-423a-a00c-3a91866e5582
+begin
+	local ε = 10.0 .^ -LinRange(1.0,16.0,100)
+	local N = 10 .^ LinRange(1.0,16.0, 100)
+	contour(N, ε, (x,y) -> log10(sqrt(x-1+y^2 - x*y^(2/x))),
+		xscale=:log10,
+		yscale=:log10,
+		fill=true,
+		xlabel=L"N",
+		ylabel=L"\varepsilon",
+		title=L"\log_{10} h",
+		levels=50,
+		grid=false
+	)
+end
+
+# ╔═╡ 837ae74e-93f4-4430-b433-8be904b098ba
+begin
+	local ε = BigFloat.([-2.5, -5.0, -10.0, -15.0])
+	local N = BigFloat.(10 .^ LinRange(1.0,20.0, 200))
+	local h = Array{BigFloat,2}(undef, length(ε), length(N))
+	f(x,y) = sqrt(x-1 + y^2 - x * y^(2/x))
+	for j in 1:length(N), i in 1:length(ε)
+		h[i,j] = f(N[j], 10.0^ε[i])
+	end
+	local p = plot(;
+		xlabel=L"N",
+		ylabel=L"h",
+		#yscale=:log10,
+		xscale=:log10
+	)
+	for i in 1:length(ε)
+		plot!(p, N, h[i,:], label=L"\varepsilon = 10^{%$(ε[i])}")
+	end
+	#savefig(p, "images/bottleneck-henrici-3.pdf")
+	p
 end
 
 # ╔═╡ Cell order:
@@ -159,3 +201,5 @@ end
 # ╠═26bb659f-3c19-42b7-aa9e-b2d40adec17c
 # ╠═3e65e0d0-37fd-4a65-97f4-9709dad4d3c8
 # ╠═645baff1-ee56-4aca-a367-f6e1ebedc661
+# ╠═00ec2f66-38b1-423a-a00c-3a91866e5582
+# ╠═837ae74e-93f4-4430-b433-8be904b098ba
